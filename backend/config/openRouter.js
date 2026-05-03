503b95ad60550aa923faf651bd7cbf13e7ebb57f
Amrit Kumar Thakur <amrit.thakur2023@gmail.com>
2026-05-03 20:28:18 +0545

    first commit

--- backend/config/openRouter.js
+++ backend/config/openRouter.js
@@ -7,7 +7,7 @@ export const generateResponse = async (prompt) => {
     const res = await fetch(openRouterUrl, {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+        Authorization: `Bearer ${process.env.AI_API_KEY}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
