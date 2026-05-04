821e493909de4254c5ade2172a39c9f3017a65f9
Amrit Kumar Thakur <amrit.thakur2023@gmail.com>
2026-05-04 21:36:35 +0545

    backend url added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // proxy API requests to the backend server
+      '/api': 'https://webnest-api.vercel.app', // proxy API requests to the backend server
     },
   },
   plugins: [react(),tailwindcss()],
